f37421f00a97bad9927e3a09b8b5e3d70a10e539
anju54 <anju.k302@gmail.com>
2020-11-09 19:18:27 +0530

    Added some minor changes in address service

---
+++ address-service/src/main/java/com/amigo/addressservice/constants/MessagingConstants.java
@@ -0,0 +1,9 @@
+package com.amigo.addressservice.constants;
+
+public class MessagingConstants {
+	
+	public static final String QUEUE = "employee-mail-queue";
+	public static final String EXCHANGE = "employee-mail-ex";
+	public static final String ROUTING_KEY = "employee-mail-key";
+
+}
--- address-service/src/main/java/com/amigo/addressservice/controller/AddressController.java
+++ address-service/src/main/java/com/amigo/addressservice/controller/AddressController.java
@@ -1,6 +1,7 @@
 package com.amigo.addressservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +26,11 @@ public class AddressController {
 							throws StateNotFound, CityNotFound, CountryNotFound {
 		
 		return addressService.addAddress(addressDto);
+	}
+	
+	@GetMapping(value = "/test")
+	public String testing() {
+		
+		return "Succefully processed!!";
 	}
 }
